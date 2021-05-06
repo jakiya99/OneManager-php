da31340a4bbb002d678267b42c078b31088a80c6
Jakiya <67457538+jakiya99@users.noreply.github.com>
2021-05-06 09:51:11 +0530

    Create jsphoto.js

---
+++ jsphoto.js
@@ -0,0 +1,67 @@
+<div id="x"></div>
+
+<script>
+var img = document.createElement("img");
+ 
+img.src = "https://i.pinimg.com/736x/49/af/22/49af220dfff88cefa3b3bdafb467453f.jpg";
+var src = document.getElementById("x");
+ 
+src.appendChild(img);
+</script>
+
+<div id="x"></div>
+
+<script>
+var img = document.createElement("img");
+ 
+img.src = "https://cdn.wallpapersafari.com/68/69/RD8OEk.jpegg";
+var src = document.getElementById("x");
+ 
+src.appendChild(img);
+</script>
+
+<div id="x"></div>
+
+<script>
+var img = document.createElement("img");
+ 
+img.src = "https://i.pinimg.com/736x/49/af/22/49af220dfff88cefa3b3bdafb467453f.jpg";
+var src = document.getElementById("x");
+ 
+src.appendChild(img);
+</script>
+
+<div id="x"></div>
+
+<script>
+var img = document.createElement("img");
+ 
+img.src = "https://cdn.wallpapersafari.com/68/69/RD8OEk.jpegg";
+var src = document.getElementById("x");
+ 
+src.appendChild(img);
+</script>
+
+<div id="x"></div>
+
+<script>
+var img = document.createElement("img");
+ 
+img.src = "https://wallpaperaccess.com/full/1567838.jpg";
+var src = document.getElementById("x");
+ 
+src.appendChild(img);
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
